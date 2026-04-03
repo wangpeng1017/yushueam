40af4f4365a20c7adf38133af6cc952e962209c5
lizhengliang <lizhengliang@iiglocal.com>
2026-04-03 13:58:48 +0800

    #update 系统标题修改

--- src/views/Login/components/LoginFormTitle.vue
+++ src/views/Login/components/LoginFormTitle.vue
@@ -2,7 +2,7 @@
   <!-- 登录、手机登录、二维码登录显示自定义标题 -->
   <div v-if="showCustomTitle" class="enter-x mb-3 text-left text-white">
     <div class="text-2xl font-bold xl:text-3xl">登录到</div>
-    <div class="text-2xl font-bold xl:text-3xl">智能制造资产管理系统</div>
+    <div class="text-2xl font-bold xl:text-3xl">资产管理系统</div>
   </div>
   <!-- 其他登录方式显示原有标题 -->
   <h2 v-else class="enter-x mb-3 text-center text-2xl font-bold xl:text-center xl:text-3xl text-white">
